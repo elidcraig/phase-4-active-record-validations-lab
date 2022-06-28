8df19387649530c8173a2589417761724c5f1dc0
Eli Craig <elidcraig@gmail.com>
2022-06-28 10:20:59 -0700

    commit tree 811e138078a0c2b01ae14c865d8aa7c2ba30844b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  # validate :title_is_clickbait
+
+  def title_is_clickbait
+    clickbait = ['Won\'t Believe', 'Secret', 'Top [number]', 'Guess']
+    is_valid = clickbait.any? { |bait| title.include? bait }
+
+    unless is_valid
+      errors.add( :title, 'Not provokative enough!' )
+    end
+  end
 end
